b9f72c47a9818173ac5450ba63216359c54d01bd
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-10 23:09:25 +0530

    Added menu section

--- src/Sections/Menu/Menu.jsx
+++ src/Sections/Menu/Menu.jsx
@@ -4,7 +4,7 @@ import Image from 'react-bootstrap/Image';
 const Menu = () => {
   return (
     <div className="Menu">
-         <Image src="https://d3kanykijpjn5y.cloudfront.net/category-33451-small-200.jpg" roundedCircle />
+         <Image src="https://d3kanykijpjn5y.cloudfront.net/category-33451-small-200.jpg"  />
     </div>
   )
 }
