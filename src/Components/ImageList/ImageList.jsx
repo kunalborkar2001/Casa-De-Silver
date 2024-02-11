0bfbb430528565e498aaee4985c3e67cbd326ae7
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-11 15:32:40 +0530

    Gallery

--- src/Components/ImageList/ImageList.jsx
+++ src/Components/ImageList/ImageList.jsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import ambience1 from '../../Assets/ambience1.jpeg'
-import ambience2 from '../../Assets/ambience2.jpeg'
-import ambience3 from '../../Assets/ambience3.jpeg'
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
@@ -14,7 +11,7 @@ function srcset(image, size, rows = 1, cols = 1) {
   };
 }
 
-export default function QuiltedImageList() {
+export default function QuiltedImageList({images}) {
   return (
     <ImageList
       sx={{ width: 500, height: 450 }}
@@ -22,7 +19,7 @@ export default function QuiltedImageList() {
       cols={4}
       rowHeight={121}
     >
-      {itemData.map((item) => (
+      {images.map((item) => (
         <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
           <img
             {...srcset(item.img, 121, item.rows, item.cols)}
@@ -35,63 +32,63 @@ export default function QuiltedImageList() {
   );
 }
 
-const itemData = [
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/908bc97625b208280f37953804ef7419.jpg',
-    title: 'Breakfast',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/6a5b428df8859e58d33c004e37d8a9c7.jpg',
-    title: 'Burger',
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/9ac89fc55573c8b22e18c15e121fb932.jpg',
-    title: 'Camera',
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/de05b0fdacc67fa44645376023b3c88b.jpg',
-    title: 'Coffee',
-    cols: 2,
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/aa33664f4555a0587dbc1ec624ef5756.jpg',
-    title: 'Hats',
-    cols: 2,
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/6d40563d85c4a80a45750f4c8cca36cd.jpg',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/b8d48b9489170645fc24b6716ff1cb0e.jpg',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/3c887b215663bc164532137b63c1a082.jpg',
-    title: 'Fern',
-  },
-  {
-    img: 'https://d3kanykijpjn5y.cloudfront.net/a9e749346f08b9989128528267691699.jpg',
-    title: 'Mushrooms',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: ambience1,
-    title: 'Tomato basil',
-  },
-  {
-    img: ambience2,
-    title: 'Sea star',
-  },
-  {
-    img: "	https://d3kanykijpjn5y.cloudfront.net/b361fad3ac3cee14d9719ce197b8069d.jpg",
-    title: 'Bike',
-    cols: 2,
-  },
-];
+// const itemData = [
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/908bc97625b208280f37953804ef7419.jpg',
+//     title: 'Breakfast',
+//     rows: 2,
+//     cols: 2,
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/6a5b428df8859e58d33c004e37d8a9c7.jpg',
+//     title: 'Burger',
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/9ac89fc55573c8b22e18c15e121fb932.jpg',
+//     title: 'Camera',
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/de05b0fdacc67fa44645376023b3c88b.jpg',
+//     title: 'Coffee',
+//     cols: 2,
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/aa33664f4555a0587dbc1ec624ef5756.jpg',
+//     title: 'Hats',
+//     cols: 2,
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/6d40563d85c4a80a45750f4c8cca36cd.jpg',
+//     title: 'Honey',
+//     author: '@arwinneil',
+//     rows: 2,
+//     cols: 2,
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/b8d48b9489170645fc24b6716ff1cb0e.jpg',
+//     title: 'Basketball',
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/3c887b215663bc164532137b63c1a082.jpg',
+//     title: 'Fern',
+//   },
+//   {
+//     img: 'https://d3kanykijpjn5y.cloudfront.net/a9e749346f08b9989128528267691699.jpg',
+//     title: 'Mushrooms',
+//     rows: 2,
+//     cols: 2,
+//   },
+//   {
+//     img: ambience1,
+//     title: 'Tomato basil',
+//   },
+//   {
+//     img: ambience2,
+//     title: 'Sea star',
+//   },
+//   {
+//     img: "	https://d3kanykijpjn5y.cloudfront.net/b361fad3ac3cee14d9719ce197b8069d.jpg",
+//     title: 'Bike',
+//     cols: 2,
+//   },
+// ];
--- src/Components/Tabs/Tabs.jsx
+++ src/Components/Tabs/Tabs.jsx
@@ -5,6 +5,8 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import QuiltedImageList from '../ImageList/ImageList';
+import ambience1 from '../../Assets/ambience1.jpeg'
+import ambience2 from '../../Assets/ambience2.jpeg'
 
 function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -39,6 +41,117 @@ export default function BasicTabs() {
         setValue(newValue);
     };
 
+    const ambienceImg = [
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/908bc97625b208280f37953804ef7419.jpg',
+          title: 'Breakfast',
+          rows: 2,
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/6a5b428df8859e58d33c004e37d8a9c7.jpg',
+          title: 'Burger',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/9ac89fc55573c8b22e18c15e121fb932.jpg',
+          title: 'Camera',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/de05b0fdacc67fa44645376023b3c88b.jpg',
+          title: 'Coffee',
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/aa33664f4555a0587dbc1ec624ef5756.jpg',
+          title: 'Hats',
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/6d40563d85c4a80a45750f4c8cca36cd.jpg',
+          title: 'Honey',
+          author: '@arwinneil',
+          rows: 2,
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/b8d48b9489170645fc24b6716ff1cb0e.jpg',
+          title: 'Basketball',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/3c887b215663bc164532137b63c1a082.jpg',
+          title: 'Fern',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/a9e749346f08b9989128528267691699.jpg',
+          title: 'Mushrooms',
+          rows: 2,
+          cols: 2,
+        },
+        {
+          img: ambience1,
+          title: 'Tomato basil',
+        },
+        {
+          img: ambience2,
+          title: 'Sea star',
+        },
+        {
+          img: "	https://d3kanykijpjn5y.cloudfront.net/b361fad3ac3cee14d9719ce197b8069d.jpg",
+          title: 'Bike',
+          cols: 2,
+        },
+      ];
+
+
+    
+      const foodImg = [
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/237a209bbcf4fdbdc51c6b192275de64.jpg',
+          title: 'Breakfast',
+          rows: 2,
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/9d86d7199a058b84417207290f74f480.jpg',
+          title: 'Burger',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/b1295cf1e7c62cdfa7c5936de0b3afc8.jpg',
+          title: 'Camera',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/2275a5172ba51e4cab39c7ce257990c9.jpg',
+          title: 'Coffee',
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/cc47a86dbf7dd4c6541b5c1429610266.jpg',
+          title: 'Hats',
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/f6ea7ea709541a2a0405a7b088676032.jpg',
+          title: 'Honey',
+          author: '@arwinneil',
+          rows: 2,
+          cols: 2,
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/4ce5c8f881c55a405bd605d057440fef.jpg',
+          title: 'Basketball',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/0f3429d2326be55ae2bb118f591c54ca.jpg',
+          title: 'Fern',
+        },
+        {
+          img: 'https://d3kanykijpjn5y.cloudfront.net/8d85bed484af776909d5a3387721e956.jpg',
+          title: 'Mushrooms',
+          rows: 2,
+          cols: 2,
+        },
+      ];
+
     return (
         <Box sx={{ textAlign : "center" }}  >
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -48,10 +161,10 @@ export default function BasicTabs() {
                 </Tabs>
             </Box>
             <CustomTabPanel value={value} index={0} >
-                <QuiltedImageList/>
+                <QuiltedImageList images={ambienceImg}/>
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
-                Item Two
+            <QuiltedImageList images={foodImg}/>
             </CustomTabPanel>
         </Box>
     );
