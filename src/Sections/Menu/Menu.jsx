4ed930f37964b75efd01672560e895578a7bc039
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-11 14:19:06 +0530

    Added responsivness till menu

--- src/Sections/Menu/Menu.jsx
+++ src/Sections/Menu/Menu.jsx
@@ -9,31 +9,31 @@ const Menu = () => {
         <h1>Menu</h1>
       </div>
       <Grid container spacing={2}>
-        <Grid xs={6} md={4} lg={3} >
+        <Grid xs={12} sm={6} md={4} lg={3} >
           <div className="gridMenu">
             <Image className="menuImg" src="https://d3kanykijpjn5y.cloudfront.net/category-33451-small-200.jpg" alt="kunalborkar2001@gmail.com" />
             <h3>Best Seller</h3>
           </div>
         </Grid>
-        <Grid xs={6} md={4} lg={3}>
+        <Grid xs={12} sm={6} md={4} lg={3}>
           <div className="gridMenu">
             <Image className="menuImg" src="https://d3kanykijpjn5y.cloudfront.net/category-32553-small-200.jpg" alt="kunalborkar2001@gmail.com" />
             <h3 className="food">Food</h3>
           </div>
         </Grid>
-        <Grid xs={6} md={4} lg={3}>
+        <Grid xs={12} sm={6} md={4} lg={3}>
           <div className="gridMenu">
             <Image className="menuImg" src="https://d3kanykijpjn5y.cloudfront.net/category-32761-small-200.jpg" alt="kunalborkar2001@gmail.com" />
             <h3 className="dessert">Desserts</h3>
           </div>
         </Grid>
-        <Grid xs={6} md={4} lg={3}>
+        <Grid xs={12} sm={6} md={4} lg={3}>
           <div className="gridMenu">
             <Image className="menuImg" src="https://d3kanykijpjn5y.cloudfront.net/category-32762-small-200.jpg" alt="kunalborkar2001@gmail.com" />
             <h3>Beverages</h3>
           </div>
         </Grid>
-        <Grid xs={6} md={4} lg={3}>
+        <Grid xs={12} sm={6} md={4} lg={3}>
           <div className="gridMenu">
             <Image className="menuImg" src="https://d3kanykijpjn5y.cloudfront.net/category-33455-small-200.jpg" alt="kunalborkar2001@gmail.com" />
             <h3 className="drinks">Drinks</h3>
