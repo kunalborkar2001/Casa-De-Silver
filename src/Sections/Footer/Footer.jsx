5c0093ec83e82f39f19e9369c2af1c35e1b608cb
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-12 10:45:37 +0530

    added some animations

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -11,12 +11,12 @@ const Footer = () => {
             <footer>
                 <div class="top">
                     <div class="pages">
-                            <img src="https://d3kanykijpjn5y.cloudfront.net/d9520615cdf9c6f9f7e6e1c168c07585.png" alt="logo" />
+                        <img src="https://d3kanykijpjn5y.cloudfront.net/d9520615cdf9c6f9f7e6e1c168c07585.png" alt="logo" />
                         <ul>
                             <li><h3>Casa De Silver</h3></li>
                             <li><h5>Average Cost</h5></li>
                             <li><p>INR 1200 for two prople (approx)</p></li>
-                            
+
                         </ul>
 
                         <ul>
@@ -27,24 +27,24 @@ const Footer = () => {
 
                         <ul>
                             <h3>Contact Us</h3>
-                            <li><a href="#"><EmailIcon/> casadesilver03@gmail.com</a></li>
-                            <li><a href="#"><WhatsAppIcon/> WhatsApp : +91 7768035252</a></li>
-                            <li><a href="#"><EditLocationAltIcon/> Aundh-Ravet Brt Road Near Dange Chowk Behind Silver Fitness Club,</a></li>
-                            <li><a href="#">Tathawade, Pune - 411033</a></li>
+                            <li><EmailIcon /> casadesilver03@gmail.com</li>
+                            <li><WhatsAppIcon /> WhatsApp : +91 7768035252</li>
+                            <li><EditLocationAltIcon /> Aundh-Ravet Brt Road Near Dange Chowk Behind Silver Fitness Club,</li>
+                            <li>Tathawade, Pune - 411033</li>
                         </ul>
                     </div>
                 </div>
                 <div class="social">
                     <i class="fab fa-linkedin"></i>
                     <i class="fab fa-github"></i>
-                    <a href="https://www.facebook.com/casadesilverpune?mibextid=ZbWKwL"><i class="fab fa-facebook"><FacebookIcon/></i></a>
-                    <a href='https://www.instagram.com/casadesilver.pune'><i class="fab fa-instagram"><InstagramIcon/></i></a>
+                    <a href="https://www.facebook.com/casadesilverpune?mibextid=ZbWKwL"><i class="fab fa-facebook"><FacebookIcon /></i></a>
+                    <a href='https://www.instagram.com/casadesilver.pune'><i class="fab fa-instagram"><InstagramIcon /></i></a>
                     <i class="fab fa-twitter"></i>
                     <i class="fab fa-youtube"></i>
                 </div>
                 <div class="info">
                     <div class="legal">
-                        <a href="#">Terms & Conditions</a><a href="#">Privacy Policy</a>
+                        <p>Terms & ConditionsPrivacy Policy</p>
                     </div>
                     <div class="copyright">2024 Copyright &copy; Kunal Borkar</div>
                 </div>
